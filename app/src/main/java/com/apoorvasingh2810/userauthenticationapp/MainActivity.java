f5dc3e2b6d7ff30e2aff0dde9d236b13bfaa5f30
Apoorva Singh <apoorva.singh2810@gmail.com>
2018-07-23 01:17:03 +0530

    Layout for register activity added with google services

--- app/src/main/java/com/apoorvasingh2810/userauthenticationapp/MainActivity.java
+++ app/src/main/java/com/apoorvasingh2810/userauthenticationapp/MainActivity.java
@@ -1,13 +1,31 @@
 package com.apoorvasingh2810.userauthenticationapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
 
+Button registerActivityBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        registerActivityBtn=(Button)findViewById(R.id.registerActivityBtn);
+        registerActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
